6afa3dba946f867297c85ef5d67d937cfce6b43c
kibochamark <regexmark1@gmail.com>
2025-04-13 21:48:27 +0300

    chat history

--- build_files.sh
+++ build_files.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo "Building project packages ...."
-python3 -m pip install -r requirements.txt
+python3 -m pip install -r requirement.txt
 
 
 echo "Migrating Database...."
